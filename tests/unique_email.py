55d46d9da933fd1b9008eff2394a852bd3e359a7
VU Anh Tuan <vuanhtuan1012@gmail.com>
2021-09-07 10:34:07 +0200

    small modification

--- tests/unique_email.py
+++ tests/unique_email.py
@@ -2,7 +2,7 @@
 # @Author: anh-tuan.vu
 # @Date:   2020-11-01 18:42:32
 # @Last Modified by:   anh-tuan.vu
-# @Last Modified time: 2020-11-02 10:15:26
+# @Last Modified time: 2021-09-07 10:16:40
 
 import unittest
 
@@ -14,24 +14,24 @@ __algo_dir = dirname(dirname(__abs_cur_file_path))
 from unique_email_addresses import unique_email as uea
 
 class UniqueEmail(unittest.TestCase):
-	__emails = ["test.email+alex@leetcode.com",
+	emails = ["test.email+alex@leetcode.com",
 	"test.e.mail+bob.cathy@leetcode.com",
 	"testemail+david@lee.tcode.com"
 	]
 
 
 	def test_solution_1(self):
-		nb_unique_emails = uea.Solution().solution_1(self.__emails)
+		nb_unique_emails = uea.Solution().solution_1(self.emails)
 		self.assertTrue(nb_unique_emails == 2)
 
 
 	def test_solution_2(self):
-		nb_unique_emails = uea.Solution().solution_2(self.__emails)
+		nb_unique_emails = uea.Solution().solution_2(self.emails)
 		self.assertTrue(nb_unique_emails == 2)
 
 
 	def test_solution_3(self):
-		nb_unique_emails = uea.Solution().solution_3(self.__emails)
+		nb_unique_emails = uea.Solution().solution_3(self.emails)
 		self.assertTrue(nb_unique_emails == 2)
 
 
